8eb5ec45efdf6f650eca996808d388768d2b0266
Kyoko ANZAI <>
2017-11-17 04:36:47 +0000

    シンボリックリンク作成時に -f オプション追加

--- create_symlink.sh
+++ create_symlink.sh
@@ -4,6 +4,6 @@ DOT_FILES=(.vimrc .gitconfig .bashrc)
 
 for file in ${DOT_FILES[@]}
     do
-        ln -s $HOME/dotfiles/$file $HOME/$file
+        ln -sf $HOME/dotfiles/$file $HOME/$file
         echo "embed: $file"
     done
