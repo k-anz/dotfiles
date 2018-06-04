11e74fa0445a1f567fac7dfdf6ba9344a0813d27
Kyoko ANZAI <vagrant@konami-default.vagrant>
2018-06-04 05:38:45 +0100

    vim-enhancedインストール削除

--- install_vim_enhanced.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# 参考 : CentOSのviでシンタックスハイライトを使えるようにする
-# https://corgi-lab.com/linux/dev-env/vi-syntax-highlight/
-
-yum install -y vim-enhanced
-mv /bin/vi /bin/vim-tiny
-ln -s /usr/bin/vim /bin/vi
