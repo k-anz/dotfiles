05e0c1c4c95240f7b29cdd2a6d80ac74727b8657
Kyoko OZAKI <>
2022-10-26 11:18:51 +0900

    不要ファイルを削除

--- install_neovim.sh
+++
@@ -1,9 +0,0 @@
-sudo yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
-git clone https://github.com/neovim/neovim
-cd neovim/
-make
-sudo make install
-mkdir -p ~/.config/nvim/
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-rm -rf ~/neovim
